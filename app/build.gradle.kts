a245fac8d7c0cb1f2d051745d66305d0933ab994
akki251 <akshansh773@gmail.com>
2024-07-14 01:49:27 +0530

    Add/Dependencies added for each library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("dagger.hilt.android.plugin")
+    // Got an error that Room doesn't support kapt, migrated to ksp
+    id("com.google.devtools.ksp")
+
 }
 
 android {
@@ -66,4 +70,47 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+
+
+    val hiltVers = "2.50"
+    implementation("com.google.dagger:hilt-android:$hiltVers")
+    implementation("com.google.dagger:hilt-android-gradle-plugin:$hiltVers")
+    ksp("com.google.dagger:hilt-compiler:$hiltVers")
+    ksp("androidx.hilt:hilt-compiler:1.1.0") // 1.0.0 in course
+    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
+    //hilt nav above was 1.0.0-alpha03 in vid
+
+    //material icons
+    implementation("androidx.compose.material:material-icons-extended:1.5.4")
+
+    // coroutines
+    val crVers = "1.7.3" // 1.5.2 in course
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${crVers}")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:${crVers}")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:${crVers}")
+
+    //coroutine lifecycle scopes
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0") //2.4.0 in course
+    // lifecycle-runtime-ktx already in auto generated dependencies,
+    // otherwise it would be here
+
+
+    //coil
+    implementation("io.coil-kt:coil-compose:2.5.0")
+
+    //retrofit & json converter
+    val rfVers = "2.9.0"
+    implementation("com.squareup.retrofit2:retrofit:$rfVers")
+    implementation("com.squareup.retrofit2:converter-gson:$rfVers")
+
+    //OkHttp
+    implementation("com.squareup.okhttp3:okhttp:4.12.0") // 5.0.0-alpha.2 in course
+
+    //Room
+    val roomVers = "2.6.1"
+    implementation("androidx.room:room-runtime:$roomVers")
+    annotationProcessor("androidx.room:room-compiler:$roomVers")
+    ksp("androidx.room:room-compiler:$roomVers")
+    implementation("androidx.room:room-ktx:$roomVers")
 }
